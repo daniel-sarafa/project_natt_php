57bdc4c18d6afaaae2d67901bac4cfdb793a8d0a
root <root@ip-172-31-25-78.us-east-2.compute.internal>
2017-11-08 07:44:55 +0000

    finished signup (needs to go to mainpage)

--- mobile/signup.php
+++ mobile/signup.php
@@ -1,6 +1,11 @@
 <?php
-@$database = new mysqli("localhost", "root", "natt", "usersDB");
-if($database->connect_error){
+
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+$mysqli = new mysqli("localhost", "root", "natt", "usersDB");
+if($mysqli->connect_error){
 	die("Connection failed.");
 }
 
@@ -22,6 +27,7 @@ if($database->connect_error){
 			<div class="theform" style="margin-top: 15%;">
 			<!-- needs to be fixed without thymeleaf -->
 			<?php
+		
 			$username = "";
 			$password = "";
 			$email = "";
@@ -43,8 +49,9 @@ if($database->connect_error){
 		</div>
 <?php
 
-if(isset($_REQUEST['username']) && isset($_REQUEST['password']) && isset($_REQUEST['email') && isset($_REQUEST['firstname']) &&
+if(isset($_REQUEST['username']) && isset($_REQUEST['password']) && isset($_REQUEST['email']) && isset($_REQUEST['firstname']) &&
 	isset($_REQUEST['lastname'])){
+
 	$username = htmlspecialchars($_REQUEST['username']);
 	$password = htmlspecialchars($_REQUEST['password']);
 	$hashed_pass = password_hash($password, PASSWORD_DEFAULT);
@@ -53,17 +60,19 @@ if(isset($_REQUEST['username']) && isset($_REQUEST['password']) && isset($_REQUE
 	$lastname = htmlspecialchars($_REQUEST['lastname']);
 	
 	$stmtUnique = 'SELECT * from users where username = "' . $username . '"';
-	$result = $database->query($stmtUnique);
+	$result = $mysqli->query($stmtUnique);
 	if($result->num_rows > 0){
 		//use print to call preventDefault in javascript function (let user know that username is taken)
 	} else {
-		$stmtinsert = $database->prepare("INSERT INTO users (username, password, firstname, lastname, email) VALUES(?, ?, ?, ?, ?)");
+		$stmtinsert = $mysqli->prepare("INSERT INTO users (username, password, firstname, lastname, email) VALUES(?, ?, ?, ?, ?)");
 		$stmtinsert->bind_param("sssss", $username, $hashed_pass, $firstname, $lastname, $email);
 		$stmtinsert->execute(); 
 		$stmtinsert->close();
 	}
-	$database->close();
+	$mysqli->close();
+
 }
+
 
 
 ?>
